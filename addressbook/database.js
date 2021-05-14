ab36caddbbf2dace95752d12de33623ce5835619
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-05-14 15:57:12 +0200

    postgres config

--- addressbook/database.js
+++ addressbook/database.js
@@ -1,6 +1,4 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../database');
+
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres', process.env.DB_USER ||'postgres',process.env.DB_PASSWORD || '', {
     host:process.env.DB_HOST || 'localhost',
--- addressbook/routes/persons.js
+++ addressbook/routes/persons.js
@@ -10,4 +10,36 @@ router.get("/all",(req,res)=>{
     })
 });
 
-router.get('/')
+router.get("/:id",(req,res)=>{
+    db.Person.findByPk(req.params.id).then(person=>{
+        res.status(200).send(JSON.stringify(person));
+    }).catch(err=>{
+        res.send(500).status(JSON.stringify(err));
+    })
+});
+
+router.put("/",(req,res)=>{
+    db.Person.create({
+        firstName:req.body.firstName,
+        lastName:req.body.lastName,
+        id:req.body.id
+    }).then(person=>{
+        res.status(2001).send(JSON.stringify(person));
+    }).catch(err=>{
+        res.status(500).send(JSON.stringify(err));
+    })
+});
+
+router.delete("/:id",(req,res)=>{
+    db.Person.destroy({
+        where:{
+            id:req.params.id
+        }
+    }).then(()=>{
+        res.status(200).send();
+    }).catch(err=>{
+        res.status(500).send(JSON.stringify(err));
+    })
+})
+
+module.exports = router;
