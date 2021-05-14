05e6a901c8da9042f46d8193d721251b380fcd3c
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-05-14 09:29:38 +0200

    added database.test file

--- addressbook/routes/persons.js
+++ addressbook/routes/persons.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../database');
+
+router.get("/all",(req,res)=>{
+    db.Person.findAll().then(persons=>{
+        res.status(200).send(JSON.stringify(persons));
+    }).catch(err=>{
+        res.status(500).send(JSON.stringify(err));
+    })
+});
+
+router.get('/')
